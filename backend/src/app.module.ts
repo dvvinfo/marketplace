2faa23a7a871e67a6a4f82685dad86b7ac10a743
Vladimir <dvvinfo@mail.ru>
2025-10-10 14:29:52 +0300

    feat: реорганизация модуля пользователя, добавление DTO и контроллера, удаление устаревших файлов

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from './config.module';
 import { TypeOrmModule } from '@db/typeorm.module';
-import { UserModule } from '@entities/user/user.module';
+import { UserModule } from '@modules/user/user.module';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule, UserModule],
